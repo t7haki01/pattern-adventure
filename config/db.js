ab8bcc2e7bcf945e050a99dcbd1c8884e468f276
t7haki01 <t7haki01@students.oamk.fi>
2019-08-14 09:20:06 +0300

    confidentails db removed

--- config/db.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'test',
-  database: 'patternadventure'
-});
-module.exports = connection;
